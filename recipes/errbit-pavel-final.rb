d19a1ab6f66d490dca6e004eec81049bedce092a
livin880 <83125779+livin880@users.noreply.github.com>
2021-05-01 18:26:45 +0200

    Update errbit-pavel-final.rb

--- recipes/errbit-pavel-final.rb
+++ recipes/errbit-pavel-final.rb
@@ -2,12 +2,12 @@
 case node['platform']
 when 'centos'|| 'redhat'
 # Call implementation recipe based on platform version
-  include_recipe 'errbit::rubyenv_centos_redhat'
-  include_recipe 'errbit::mongoDB_centos'
-  include_recipe 'errbit::errbit_final_centos'
+  include_recipe 'errbit-pavel-cookbook::rubyenv_centos_redhat'
+  include_recipe 'errbit-pavel-cookbook::mongoDB_centos'
+  include_recipe 'errbit-pavel-cookbook::errbit_final_centos'
 
 when 'ubuntu'
-include_recipe 'errbit::rubyenv_ubuntu'
-include_recipe 'errbit::mongoDB_ubuntu'
-include_recipe 'errbit::errbit_final_ubuntu'
+include_recipe 'errbit-pavel-cookbook::rubyenv_ubuntu'
+include_recipe 'errbit-pavel-cookbook::mongoDB_ubuntu'
+include_recipe 'errbit-pavel-cookbook::errbit_final_ubuntu'
 end
